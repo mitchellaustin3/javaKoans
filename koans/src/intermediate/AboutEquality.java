a8f7cdd455439df1c2cb50efa089aa43d9a5ff98
Austin Mitchell <mitchellaustin3@gmail.com>
2017-11-24 22:01:20 -0800

    Finished beginner Koans, starrted int

--- koans/src/intermediate/AboutEquality.java
+++ koans/src/intermediate/AboutEquality.java
@@ -12,20 +12,20 @@ public class AboutEquality {
     public void sameObject() {
         Object a = new Object();
         Object b = a;
-        assertEquals(a == b, __);
+        assertEquals(a == b, true);
     }
 
     @Koan
     public void equalObject() {
         Integer a = new Integer(1);
         Integer b = new Integer(1);
-        assertEquals(a.equals(b), __);
-        assertEquals(b.equals(a), __);
+        assertEquals(a.equals(b), true);
+        assertEquals(b.equals(a), true);
     }
 
     @Koan
     public void noObjectShouldBeEqualToNull() {
-        assertEquals(new Object().equals(null), __);
+        assertEquals(new Object().equals(null), false);
     }
 
     static class Car {
@@ -39,7 +39,18 @@ public class AboutEquality {
 
         @Override
         public boolean equals(Object other) {
-            // Change this implementation to match the equals contract
+			
+			if (other == null){
+				if (this == null)
+					return true;
+				return false;
+			}
+			if (other.getClass() !=  Car.class)
+				return false;
+			Car o = (Car) other;
+            if (this.name == o.name && this.horsepower == o.horsepower)
+				return true;
+			// Change this implementation to match the equals contract
             // Car objects with same horsepower and name values should be considered equal
             // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#equals(java.lang.Object)
             return false;
@@ -48,7 +59,7 @@ public class AboutEquality {
         @Override
         public int hashCode() {
             // @see http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
-            return super.hashCode();
+            return this.name.length() + this.horsepower;
         }
     }
 
@@ -103,7 +114,12 @@ public class AboutEquality {
 
         @Override
         public int hashCode() {
-            return 4000;
+			int hash = 0;
+			char[] charArray = this.color.toCharArray();
+            for (int a: charArray) {
+				hash += (int)a;
+			}
+			return hash;
         }
 
         @Override
@@ -119,8 +135,8 @@ public class AboutEquality {
         Chicken chicken1 = new Chicken();
         chicken1.color = "black";
         Chicken chicken2 = new Chicken();
-        assertEquals(chicken1.equals(chicken2), __);
-        assertEquals(chicken1.hashCode() == chicken2.hashCode(), __);
+        assertEquals(chicken1.equals(chicken2), false);
+        assertEquals(chicken1.hashCode() == chicken2.hashCode(), false);
         // Does this still fit the hashCode contract? Why (not)?
         // Fix the Chicken class to correct this.
     }
--- koans/src/intermediate/AboutFileIO.java
+++ koans/src/intermediate/AboutFileIO.java
@@ -13,16 +13,16 @@ public class AboutFileIO {
     @Koan
     public void fileObjectDoesntCreateFile() {
         File f = new File("foo.txt");
-        assertEquals(f.exists(), __);
+        assertEquals(f.exists(), false);
     }
 
     @Koan
     public void fileCreationAndDeletion() throws IOException {
         File f = new File("foo.txt");
         f.createNewFile();
-        assertEquals(f.exists(), __);
+        assertEquals(f.exists(), true);
         f.delete();
-        assertEquals(f.exists(), __);
+        assertEquals(f.exists(), false);
     }
 
     @Koan
@@ -39,9 +39,9 @@ public class AboutFileIO {
         size = fr.read(in);
         // No flush necessary!
         fr.close();
-        assertEquals(size, __);
+        assertEquals(size, 22);
         String expected = new String(in);
-        assertEquals(expected.length(), __);
+        assertEquals(expected.length(), 50);
         assertEquals(expected, __);
         file.delete();
     }
